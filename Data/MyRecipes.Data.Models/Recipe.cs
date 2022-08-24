12bee6f5e5dbf2846367b5008b2e15c6f1249cc5
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-08-24 15:58:22 +0300

    Vote model added

--- Data/MyRecipes.Data.Models/Recipe.cs
+++ Data/MyRecipes.Data.Models/Recipe.cs
@@ -11,6 +11,7 @@
         {
             this.Ingredients = new HashSet<RecipeIngredient>();
             this.Images = new HashSet<Image>();
+            this.Votes = new HashSet<Vote>();
         }
 
         public string Name { get; set; }
@@ -34,5 +35,7 @@
         public virtual ICollection<RecipeIngredient> Ingredients { get; set; }
 
         public virtual ICollection<Image> Images { get; set; }
+
+        public virtual ICollection<Vote> Votes { get; set; }
     }
 }
